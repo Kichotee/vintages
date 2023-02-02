ea853869cf52adf20d829d9bd9fb28e6087b96b3
Kichotee <pt.timone@gmail.com>
2023-02-02 01:11:33 +0100

    vuetify-nuxt boilerplate

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-app  class="w-screen h-screen max-h-auto">
+		<div class="border-b w-max  h-8">
+			<!-- <v-app-bar-nav-icon class='w-25%'/> -->
+			<v-sheet  class="d-flex bg-gray flex-row  justify-space-between align-center" > 
+                <h2>The vintages</h2>
+                <p>Experience retro class</p>
+            </v-sheet>
+		</div>
+
+		<slot />
+	</v-app>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import '../assets/_reset.scss'
+
+</style>
